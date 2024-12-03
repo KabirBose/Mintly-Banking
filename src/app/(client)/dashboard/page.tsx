584bf75d6f1122a8ef57240590a460eb5656870a
Kabir Bose <kabirbose04@gmail.com>
2024-12-02 21:28:42 -0500

    last working commit

--- src/app/(client)/dashboard/page.tsx
+++ src/app/(client)/dashboard/page.tsx
@@ -1,3 +1,8 @@
+import FetchBalance from "@/components/FetchBalance";
+import FetchIdentity from "@/components/FetchIdentity";
+import FetchRecurring from "@/components/FetchRecurring";
+import FetchTransactions from "@/components/FetchTransactions";
+
 export default function () {
   return (
     <div className="flex flex-col gap-4 justify-center items-center h-[100vh]">
