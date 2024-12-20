061f06d1b8db0b5fb3a8e63fd2bcd72fcd0217ef
Kabir Bose <kabirbose04@gmail.com>
2024-12-19 20:47:43 -0500

    updated desc

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -5,8 +5,8 @@ import "@/app/globals.css";
 const nunito = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Mintly Banking",
-  description: "A comprehensive personal banking app",
+  title: "Mintly Finance",
+  description: "A comprehensive personal finance app",
 };
 
 export default function RootLayout({
